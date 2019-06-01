20549b318c2c218b48420bcb705f1e6c68b82ba1
Magnus Remoe <mremoe@gmail.com>
2019-06-01 21:30:31 +0800

    Added 5 tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#full_name return the capitalized first name and last name" do
+    user = User.new(first_name: 'jan', last_name: 'lennon')
+    assert_equal "Jan Lennon", user.full_name
+  end
+
+  test "#full_name should not crash if first_name is nil" do
+    user = User.new(last_name: 'lennon')
+    assert_equal "Lennon", user.full_name
+  end
 end
